9535842266b0b43f3571a605ae1db132584261e1
Davide <69810644+ItzDavi@users.noreply.github.com>
2021-04-28 08:59:21 +0200

    Refactored main.js

--- v2.0/public/js/main.js
+++ v2.0/public/js/main.js
@@ -1,11 +1,7 @@
 import SpriteSheet from './spritesheet.js';
 import {loadImage, loadLevel} from './loader.js';
 
-const canvas = document.getElementById('screen');
-const ctx = canvas.getContext('2d');
-
 function drawBackground(background, ctx, sprites) {
-
   background.ranges.forEach(([x1, x2, y1, y2]) => {
     for (let x = x1; x < x2; ++x) {
       for (let y = y1; y < y2; ++y) {
@@ -15,14 +11,33 @@ function drawBackground(background, ctx, sprites) {
   });
 }
 
-loadImage('assets/tiles.png').then(image => {
-  const sprites = new SpriteSheet(image);
-  sprites.define('ground', 0, 0);
-  sprites.define('sky', 3, 23);
-
-  loadLevel('1-1').then(level => {
-    level.backgrounds.forEach(bg => {
-      drawBackground(bg, ctx, sprites);
-    });
+function loadMarioSprite () {
+  return loadImage('assets/characters.gif').then(image => {
+    const sprites = new SpriteSheet(image, 16, 16);
+    sprites.define('idle', 276, 44, 16, 16);
+    return sprites;
   });
+}
+
+function loadBackgroundSprites () {
+  return loadImage('assets/tiles.png').then(image => {
+    const sprites = new SpriteSheet(image, 16, 16);
+    sprites.defineTile('ground', 0, 0);
+    sprites.defineTile('sky', 3, 23);
+    return sprites;
+  });
+}
+
+const canvas = document.getElementById('screen');
+const ctx = canvas.getContext('2d');
+
+Promise.all([
+  loadMarioSprite(),
+  loadBackgroundSprites(),
+  loadLevel('1-1'),
+]).then(([marioSprite, sprites, level]) => {
+  level.backgrounds.forEach(background => {
+    drawBackground(background, ctx, sprites);
+  });
+  marioSprite.draw('idle', ctx, 64, 64)
 });
