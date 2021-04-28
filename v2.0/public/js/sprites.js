292fbd1d53dcaf8cadc169b73faf181d56d4695c
Davide <69810644+ItzDavi@users.noreply.github.com>
2021-04-28 09:01:52 +0200

    Moved sprites function to the correct .js

---
+++ v2.0/public/js/sprites.js
@@ -0,0 +1,16 @@
+function loadMarioSprite () {
+  return loadImage('assets/characters.gif').then(image => {
+    const sprites = new SpriteSheet(image, 16, 16);
+    sprites.define('idle', 276, 44, 16, 16);
+    return sprites;
+  });
+}
+
+function loadBackgroundSprites () {
+  return loadImage('assets/tiles.png').then(image => {
+    const sprites = new SpriteSheet(image, 16, 16);
+    sprites.defineTile('ground', 0, 0);
+    sprites.defineTile('sky', 3, 23);
+    return sprites;
+  });
+}
