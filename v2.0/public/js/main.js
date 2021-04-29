5a9cc069897e6048f413a4c65330837e50f301bd
Davide <69810644+ItzDavi@users.noreply.github.com>
2021-04-29 22:19:22 +0200

    Cleaned code and moved load functions to loader.js

--- v2.0/public/js/main.js
+++ v2.0/public/js/main.js
@@ -4,25 +4,21 @@ import Timer from './timer.js';
 import Keyboard from './keyboardstate.js';
 import {loadLevel} from './loader.js';
 import {createMario} from './entities.js';
-import {loadBackgroundSprites} from './sprites.js';
-import {createBackgroundLayer, createSpriteLayer} from './layers.js';
+
+
 
 const canvas = document.getElementById('screen');
 const ctx = canvas.getContext('2d');
 
 Promise.all([
   createMario(),
-  loadBackgroundSprites(),
   loadLevel('1-1'),
-]).then(([mario, backgroundSprites, level]) => {
-  const comp = new Compositor();
+]).then(([mario, level]) => {
 
-  const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
-  comp.layers.push(backgroundLayer);
+
 
   const gravity = 2000;
   mario.pos.set(64, 180);
-  mario.vel.set(200, -600);
 
   const SPACE = 32;
   const input = new Keyboard();
@@ -35,13 +31,11 @@ Promise.all([
   });
   input.listenTo(window);
 
-  const spriteLayer = createSpriteLayer	(mario);
-  comp.layers.push(spriteLayer);
 
   const timer = new Timer(1/60);
   timer.update = function update(deltaTime) {
     mario.update(deltaTime);
-    comp.draw(ctx);
+    level.comp.draw(ctx);
     mario.vel.y += gravity * deltaTime;
   }
 
