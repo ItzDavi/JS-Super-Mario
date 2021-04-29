061eb887e8f947c9e7641fa43a5c63fde8eff780
Davide <69810644+ItzDavi@users.noreply.github.com>
2021-04-29 21:08:46 +0200

    Refactored entities.js

--- v2.0/public/js/entities.js
+++ v2.0/public/js/entities.js
@@ -1,17 +1,17 @@
 import Entity from './entity.js';
+import Velocity from './traits/velocity.js';
+import Jump from './traits/jump.js';
 import {loadMarioSprite} from './sprites.js';
 
 export function createMario() {
   return loadMarioSprite().then(sprite => {
     const mario = new Entity();
 
+    mario.addTrait(new Velocity());
+    mario.addTrait(new Jump());
+
     mario.draw = function drawMario(ctx) {
       sprite.draw('idle', ctx, this.pos.x, this.pos.y);
-    }
-
-    mario.update = function updateMario(deltaTime){
-      this.pos.x += this.vel.x * deltaTime;
-      this.pos.y += this.vel.y * deltaTime;
     }
 
     return mario;
