384300d2736960cb8ea3f0d167394f99465bebb6
Davide <69810644+ItzDavi@users.noreply.github.com>
2021-04-29 10:03:48 +0200

    Refactored main.js

--- v2.0/public/js/main.js
+++ v2.0/public/js/main.js
@@ -1,9 +1,17 @@
 import Compositor from './compositor.js';
 import Entity from './entity.js';
+import Timer from './timer.js';
+import Keyboard from './keyboardstate.js';
 import {loadLevel} from './loader.js';
 import {createMario} from './entities.js';
 import {loadBackgroundSprites} from './sprites.js';
 import {createBackgroundLayer, createSpriteLayer} from './layers.js';
+
+const input = new Keyboard();
+input.addMapping(32, keyState => {
+  console.log(keyState);
+})
+input.listenTo(window);
 
 const canvas = document.getElementById('screen');
 const ctx = canvas.getContext('2d');
@@ -16,17 +24,21 @@ Promise.all([
   const comp = new Compositor();
 
   const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
-  //comp.layers.push(backgroundLayer);
+  comp.layers.push(backgroundLayer);
 
-  const gravity = 30;
+  const gravity = 2000;
   mario.pos.set(64, 180);
   mario.vel.set(200, -600);
 
   const spriteLayer = createSpriteLayer	(mario);
   comp.layers.push(spriteLayer);
 
-  const deltaTime = 1/60;
-  
+  const timer = new Timer(1/60);
+  timer.update = function update(deltaTime) {
+    mario.update(deltaTime);
+    comp.draw(ctx);
+    mario.vel.y += gravity * deltaTime;
+  }
 
-  update(0);
+  timer.start();
 });
